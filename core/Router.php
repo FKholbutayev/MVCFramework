299c317e72edb6cd972054fb168b453ebbb100a0
FKholbutayev <ht.uzpharma@gmail.com>
2021-07-27 10:16:16 +0300

    layout content

--- core/Router.php
+++ core/Router.php
@@ -41,13 +41,24 @@ class Router {
     public function renderView($view)
     {
         $layoutContent = $this->layoutContent();
+        $viewContent = $this->renderOnlyView($view);
+
+        return str_replace('{{content}}', $viewContent, $layoutContent);
+
+    }
+
+    public function renderOnlyView($view)
+    {
+        ob_start();
         include_once Application::$ROOT_DIR."/views/$view.php";
+        return ob_get_clean();
     }
 
     public function layoutContent()
     {
+        ob_start();
         include_once Application::$ROOT_DIR."/views/layouts/main.php";
-
+        return ob_get_clean();
     }
 
 }
